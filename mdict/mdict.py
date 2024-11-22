ee76cafc8a7adfc7f0c92ecf483e70138d33b203
dax <dax26d@gmail.com>
2024-11-22 16:48:48 +0800

    新建Mdict类

---
+++ mdict/mdict.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+# mdict.py
+# 本程序参考Xiaoqiang Wang的readmdict.py，在此特别致谢。
+# created on 2024-11-22
+
+
+class Mdict:
+    def __init__(self, mdx_path: str) -> None:
+        self.mdx_path = mdx_path
