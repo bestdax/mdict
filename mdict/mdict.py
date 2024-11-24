325b6df17f9ba2837c74882724c6593f51b7cc3d
dax <dax26d@gmail.com>
2024-11-24 12:31:55 +0800

    完成键区块信息的解码

--- mdict/mdict.py
+++ mdict/mdict.py
@@ -7,6 +7,7 @@ from struct import pack, unpack
 import zlib
 import re
 import html
+from io import BytesIO
 
 
 class Mdict:
@@ -22,8 +23,21 @@ class Mdict:
         self.header_bites = None
         self.key_block_offset = 0
         self.stylesheet = {}
+        self._mdx_file = None
+        self._encoding = None
 
-    def _read_mdx_head(self):
+        self._read_mdx_header()
+        self._read_mdx_keys()
+
+    def _read_number(self, bytes_stream: BytesIO):
+        """
+        读取一个4个字节无符号整数。
+        :param bytes_stream:
+        :return:
+        """
+        return unpack(self._number_format, bytes_stream.read(self._number_width))[0]
+
+    def _read_mdx_header(self):
         """
         读取MDX文件的头部信息。
 
@@ -43,7 +57,6 @@ class Mdict:
 
                 # 记录键开始的位置
                 self.key_block_offset = mdict_file.tell()
-                mdict_file.close()
         except FileNotFoundError:
             print(f"File {self.mdx_path} not found.")
         except IOError as e:
@@ -54,20 +67,21 @@ class Mdict:
 
         # 提取tag信息并转换成字典，同时将信息逆转义 (将&lt;替换成<，将&gt;替换成>等)
         tag_list = re.findall(r'(\w+)="(.*?)"', header_text, re.DOTALL)
-        tag_dict = {key: html.unescape(value) for key, value in tag_list}
+        self._tag_dict = {key: html.unescape(value) for key, value in tag_list}
 
-        # 现在utf-8编码已经非常普遍，所以不对编码进行任何处理
+        # 现在UTF-8的编码已经非常普遍，所以不对编码进行任何处理
 
         # 读取标题和描述
-        self.title = tag_dict['title'] if 'title' in tag_dict else ''
-        self.description = tag_dict['Description'] if 'Description' in tag_dict else ''
+        self._title = self._tag_dict['title'] if 'title' in self._tag_dict else ''
+        self._description = self._tag_dict['Description'] if 'Description' in self._tag_dict else ''
+        self._encoding = self._tag_dict['Encoding'] if 'Encoding' in self._tag_dict else 'UTF-8'
 
         # 读取加密信息
         # 0：没有加密。
         # 1：关键词头被加密。
         # 2：关键词索引被加密。
         # 3：关键词头和关键词索引都被加密。这是上述两种加密的组合。
-        self._encrypted = tag_dict['encrypted'] if 'encrypted' in tag_dict else 0
+        self._encrypted = self._tag_dict['encrypted'] if 'encrypted' in self._tag_dict else 0
 
         # 摘自mdxbuilder文档
         # 记号文件的格式：
@@ -92,13 +106,13 @@ class Mdict:
         # </font>
         # 这个样式单我觉得不太实际，编写起来太麻烦了
 
-        if tag_dict.get('Stylesheet') is not None:
-            lines = tag_dict['Stylesheet'].split()
+        if self._tag_dict.get('Stylesheet') is not None:
+            lines = self._tag_dict['Stylesheet'].split()
             for row in range(0, len(lines), 3):
                 self.stylesheet[lines[row]] = lines[row + 1:row + 3]
 
         # 版本2.0之前数字的宽度是4个字节，>=2.0的版本是8个字节
-        self._version = float(tag_dict['GeneratedByEngineVersion'])
+        self._version = float(self._tag_dict['GeneratedByEngineVersion'])
         if self._version < 2.0:
             self._number_width = 4
             self._number_format = '>I'
@@ -106,7 +120,128 @@ class Mdict:
             self._number_width = 8
             self._number_format = '>Q'
 
+    def _read_mdx_keys(self) -> None:
+        with open(self.mdx_path, 'rb') as mdict_file:
+            mdict_file.seek(self.key_block_offset)
+
+            if self._version < 2.0:
+                number_bytes = self._number_width * 4
+            else:
+                number_bytes = self._number_width * 5
+
+            block = mdict_file.read(number_bytes)
+
+            # 不支持加密的MDX文件
+            if self._encrypted != 0:
+                raise NotImplementedError('不支持加密的MDX文件')
+
+            bytes_stream = BytesIO(block)
+
+            # 读取键区块的数量
+            number_of_key_blocks = self._read_number(bytes_stream)
+            # 读取词条的数量
+            self.number_of_entries = self._read_number(bytes_stream)
+            # 在2.0版本及之后，block的第三段存储键区块信息解压后的字节数
+            if self._version >= 2.0:
+                key_block_info_decompressed_size = self._read_number(bytes_stream)
+            # 键区块信息的解压缩之前的字节数
+            key_block_info_compressed_size = self._read_number(bytes_stream)
+            # 键区块的字节数
+            key_block_size = self._read_number(bytes_stream)
+
+            # 2.0版本及之后，下面的4个字节是前面5个字节的adler32校验码
+            if self._version >= 2.0:
+                adler32 = unpack('>I', mdict_file.read(4))[0]  # 以大端数存储的4位校验码
+                assert adler32 == zlib.adler32(block) & 0xffffffff
+
+            key_block_info = mdict_file.read(key_block_info_compressed_size)
+            self._decode_key_block_info(key_block_info)
+
+    def _decode_key_block_info(self, key_block_info_compressed: bytes) -> None:
+        """
+        解码压缩的键区块信息。
+
+        该函数负责将压缩的键区块信息解压，并验证其完整性。它首先检查压缩数据的头部，以确定其版本和有效性。
+        如果版本高于2.0，它将使用 zlib 库进行解压，并验证解压后数据的 Adler-32 校验码。
+        然后，根据版本不同，使用不同的格式解析键区块信息中的条目数量。
+
+        参数：
+        key_block_info_compressed (bytes): 压缩的键区块信息数据。
+
+        返回：
+        None
+
+        异常：
+        AssertionError: 如果压缩数据的头部不匹配预期的版本标记，或者 Adler-32 校验码不匹配。
+
+        注意：
+        该函数假设传入的 key_block_info_compressed 是有效的压缩数据，且版本标记和校验码正确。
+        该函数不返回任何值，而是直接修改类的内部状态。
+
+        依赖：
+        zlib: 用于解压压缩数据。
+        struct: 用于解析二进制数据。
+        """
+        if self._version > 2.0:
+            assert key_block_info_compressed[:4] == b'\x00\x00\x00\x02'
+
+            key_block_info = zlib.decompress(key_block_info_compressed[8:])
+            adler32 = unpack('>I', key_block_info_compressed[4:8])[0]  # 以大端数存储的4位校验码
+            assert adler32 == zlib.adler32(key_block_info) & 0xffffffff
+        else:
+            key_block_info = key_block_info_compressed
+
+        # 解码
+        key_block_info_list = []
+        number_of_entries = 0
+        if self._version >= 2:
+            byte_format = '>H'  # 大端2个字节无符号整数
+            byte_width = 2
+            text_term = 1
+        else:
+            byte_format = '>B'  # 大端1个字节无符号整数
+            byte_width = 1
+            text_term = 0
+
+        key_index = 0
+
+        while key_index < len(key_block_info):
+            # 当前区块中的词条数量
+            number_of_entries = unpack(self._number_format, key_block_info[key_index:key_index + self._number_width])[0]
+            key_index += self._number_width
+
+            # 文本头部长度
+            text_head_size = unpack(byte_format, key_block_info[key_index:key_index + byte_width])[0]
+            key_index += byte_width
+
+            # 文本头部
+            if self._encoding != 'UTF-16':
+                key_index += text_head_size + text_term
+            else:
+                key_index += (text_head_size + text_term) * 2
+
+            # 文本尾部长度
+            text_tail_size = unpack(byte_format, key_block_info[key_index:key_index + byte_width])[0]
+            key_index += byte_width
+
+            # 文本尾部
+            if self._encoding != 'UTF-16':
+                key_index += text_tail_size + text_term
+            else:
+                key_index += (text_tail_size + text_term) * 2
+
+            # 键区块的压缩后的字节数
+            key_block_compressed_size = \
+                unpack(self._number_format, key_block_info[key_index:key_index + self._number_width])[0]
+            key_index += self._number_width
+
+            # 键区块的解压后的字节数
+            key_block_decompressed_size = \
+                unpack(self._number_format, key_block_info[key_index:key_index + self._number_width])[0]
+            key_index += self._number_width
+            key_block_info_list.append((key_block_compressed_size, key_block_decompressed_size))
+        print(key_block_info_list)
+
 
 if __name__ == '__main__':
     mdict = Mdict('/Users/dax/Documents/Personal/Dictionary/牛津高阶9/牛津高阶英汉双解词典(第9版)_V3.1.2版.mdx')
-    mdict._read_mdx_head()
